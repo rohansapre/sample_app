cd5e3a8c0ce209fdfdc854f9840b4850923013ed
rohansapre <11bit025@nirmauni.ac.in>
2016-02-24 16:44:22 +0530

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "About | Sample App"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Sample App"
+  end
+
 end
