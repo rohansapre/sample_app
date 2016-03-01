8db581a01fde09df82fa0fc4ee44bb6d2527d730
rohansapre <11bit025@nirmauni.ac.in>
2016-03-02 01:32:37 +0530

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   get 'users/new'
 
   # You can have the root of your site routed with "root"
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
