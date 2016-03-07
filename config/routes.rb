0af1cb10b4e7603bf079231dc058f9019d9ac14b
rohansapre <11bit025@nirmauni.ac.in>
2016-03-08 00:46:10 +0530

    Add password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  
-  get 'sessions/new'
-
-  get 'users/new'
-
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
@@ -15,6 +10,7 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
