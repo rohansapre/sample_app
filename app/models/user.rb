0da627c01a9cdac69b869e1d345357b0420ccd65
Rohan Sapre <sapre.r@husky.neu.edu>
2016-10-09 19:58:41 -0400

    updated the account activation and account mailer files

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,7 @@ class User < ActiveRecord::Base
 	end
 
 	def activate
-		update_attribute(:activated, true)
-		update_attribute(:activated_at, Time.zone.now)
+		update_columns(activated: true, activated_at: Time.zone.now)
 	end
 
 	def send_activation_email
